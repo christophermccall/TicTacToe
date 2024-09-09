406553b44d3027d710eabffbfda3a75d05424fa1
Christopher McCall <camccall24@gmail.com>
2024-09-09 16:19:25 -0400

    diagnol tests passed

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,23 +4,73 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+    Character[][] matrix;
+    String winner = "";
+    Board board;
     public Board(Character[][] matrix) {
+        this.matrix = matrix;
     }
 
     public Boolean isInFavorOfX() {
-        return null;
+        int diagLToR = 0;
+        int diagRToL = 0;
+        int col = 0;
+        int row = 0;
+
+        boolean inFavor = false;
+        for (int i = 0, j = matrix.length-1; i < matrix.length; i++, j--){
+            if (matrix[j][i].toString().equals("X")){
+                diagLToR += 1;
+            }else if (matrix[i][i].toString().equals("X")){
+                diagRToL += 1;
+            }else if (matrix[0][i].toString().equals("X")){
+                row += 1;
+            } else if (matrix[i][0].toString().equals("X")){
+                col += 1;
+            }
+    }   if (diagLToR == 3|| diagRToL == 3 || col == 3 || row ==3){
+                inFavor = true;
+            }
+        return inFavor;
     }
 
     public Boolean isInFavorOfO() {
-        return null;
+        int diagLToR = 0;
+        int diagRToL = 0;
+        int col = 0;
+        int row = 0;
+
+        boolean inFavor = false;
+        for (int i = 0, j = matrix.length-1; i < matrix.length; i++, j--){
+            if (matrix[j][i].toString().equals("O")){
+                diagLToR += 1;
+            }else if (matrix[i][i].toString().equals("O")){
+                diagRToL += 1;
+            }else if (matrix[0][i].toString().equals("O")){
+                row += 1;
+            } else if (matrix[i][0].toString().equals("O")){
+                col += 1;
+            }
+    }   if (diagLToR == 3|| diagRToL == 3 || col == 3 || row ==3){
+                inFavor = true;
+            }
+        return inFavor;
     }
+
 
     public Boolean isTie() {
         return null;
     }
 
     public String getWinner() {
-        return null;
+        if(isInFavorOfX()) {
+            this.winner = "X";
+        } else if(isInFavorOfO()) {
+            this.winner = "O";
+        }
+        return this.winner;
+        }
     }
 
-}
+
+
